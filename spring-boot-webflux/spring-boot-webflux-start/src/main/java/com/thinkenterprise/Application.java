c52a3515accdb7b424723ac565f9ffbbcbf3f4f1
Thinkenterprise <michael.schaefer@msg-systems.com>
2019-08-03 17:32:34 +0200

    Update & Bug Fixes Reactive

--- spring-boot-webflux/spring-boot-webflux-start/src/main/java/com/thinkenterprise/Application.java
+++ spring-boot-webflux/spring-boot-webflux-start/src/main/java/com/thinkenterprise/Application.java
@@ -23,10 +23,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ReactiveApplication {
+public class Application {
 
     public static void main(String[] args) {
-    	SpringApplication.run(ReactiveApplication.class, args);
+    	SpringApplication.run(Application.class, args);
        
    }
 }
